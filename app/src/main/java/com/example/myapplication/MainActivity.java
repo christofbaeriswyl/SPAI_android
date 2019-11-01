23ec28c05ac0e6d600e7d440b6222b7f4b282e4e
Christof Baeriswyl <christof.baeriswyl@#student.kuleuven.be>
2019-11-01 12:05:04 +0100

    release 1.0

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,182 +1,3 @@
-/*package com.example.myapplication;
-
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-import android.os.Handler;
-import android.os.Message;
-//import android.support.v7.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.example.myapplication.UdpClientThread;
-
-public class MainActivity extends AppCompatActivity {
-
-    EditText editTextAddress, editTextPort;
-    Button buttonConnect;
-    TextView textViewState, textViewRx;
-    ImageButton imageButton;
-    ImageButton imageButton2;
-    SoundMeter soundMeter;
-    TextView tv;
-    Button btnRefreshAmpl;
-
-    UdpClientHandler udpClientHandler;
-    UdpClientThread udpClientThread;
-
-    private String ip;
-    private int port;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editTextAddress = (EditText) findViewById(R.id.address);
-        editTextPort = (EditText) findViewById(R.id.port);
-        buttonConnect = (Button) findViewById(R.id.connect);
-        textViewState = (TextView)findViewById(R.id.state);
-        textViewRx = (TextView)findViewById(R.id.received);
-
-        imageButton = findViewById(R.id.imageButton);
-        imageButton2 = findViewById(R.id.imageButton2);
-        tv = (TextView)findViewById(R.id.textView3);
-
-        buttonConnect.setOnClickListener(buttonConnectOnClickListener);
-        udpClientHandler = new UdpClientHandler(this);
-        soundMeter = new SoundMeter();
-
-        btnRefreshAmpl = (Button)findViewById(R.id.refresh_amplitude);
-        btnRefreshAmpl.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                tv.setText(Double.toString(soundMeter.getAmplitude()));
-            }
-        });
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                soundMeter.start();
-                tv.setText(Double.toString(soundMeter.getAmplitude()));
-                //tv.setText("blabla");
-            }
-        });
-
-        imageButton2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                soundMeter.stop();
-            }
-        });
-
-    }
-
-    View.OnClickListener buttonConnectOnClickListener =
-            new View.OnClickListener() {
-
-                @Override
-                public void onClick(View arg0) {
-
-                    udpClientThread = new UdpClientThread(
-                            editTextAddress.getText().toString(),
-                            Integer.parseInt(editTextPort.getText().toString()),
-                            udpClientHandler, soundMeter);
-                    udpClientThread.start();
-
-                    buttonConnect.setEnabled(false);
-                }
-            };
-
-    private void updateState(String state){
-        textViewState.setText(state);
-    }
-
-    private void updateRxMsg(String rxmsg){
-        textViewRx.append(rxmsg + "\n");
-    }
-
-    private void clientEnd(){
-        udpClientThread = null;
-        textViewState.setText("clientEnd()");
-        buttonConnect.setEnabled(true);
-
-    }
-
-    public static class UdpClientHandler extends Handler {
-        public static final int UPDATE_STATE = 0;
-        public static final int UPDATE_MSG = 1;
-        public static final int UPDATE_END = 2;
-        private MainActivity parent;
-
-        public UdpClientHandler(MainActivity parent) {
-            super();
-            this.parent = parent;
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-
-            switch (msg.what){
-                case UPDATE_STATE:
-                    parent.updateState((String)msg.obj);
-                    break;
-                case UPDATE_MSG:
-                    parent.updateRxMsg((String)msg.obj);
-                    break;
-                case UPDATE_END:
-                    parent.clientEnd();
-                    break;
-                default:
-                    super.handleMessage(msg);
-            }
-
-        }
-    }
-
-    public static class SoundMeter {
-
-        private AudioRecord ar = null;
-        public int minSize;
-
-        public void start() {
-            minSize= AudioRecord.getMinBufferSize(16000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-            ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 16000,AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,minSize);
-            //ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,minSize);
-            ar.startRecording();
-        }
-
-        public void stop() {
-            if (ar != null) {
-                ar.stop();
-            }
-        }
-
-        public double getAmplitude() {
-            short[] buffer = new short[minSize];
-            ar.read(buffer, 0, minSize);
-            int max = 0;
-            for (short s : buffer)
-            {
-                if (Math.abs(s) > max)
-                {
-                    max = Math.abs(s);
-                }
-            }
-            return max;
-        }
-
-        public void getData(short[] buffer) {
-            ar.read(buffer, 0, minSize);
-        }
-
-    }
-}
-*/
-
 package com.example.myapplication;
 
         import java.io.IOException;
@@ -194,9 +15,13 @@ package com.example.myapplication;
         import android.view.View;
         import android.view.View.OnClickListener;
         import android.widget.Button;
+        import android.widget.ImageButton;
+        import android.widget.EditText;
 
 public class MainActivity extends Activity {
     private Button startButton,stopButton;
+    private ImageButton imageStart, imageStop;
+    EditText editTextAddress, editTextPort;
 
     public byte[] buffer;
     public static DatagramSocket socket;
@@ -216,31 +41,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startButton = (Button) findViewById (R.id.start_button);
-        stopButton = (Button) findViewById (R.id.stop_button);
+        imageStart = findViewById(R.id.imageButton);
+        imageStop = findViewById(R.id.imageButton2);
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPort = (EditText) findViewById(R.id.port);
 
-        startButton.setOnClickListener (startListener);
-        stopButton.setOnClickListener (stopListener);
+        imageStart.setOnClickListener (imageStartListener);
+        imageStop.setOnClickListener (imageStopListener);
 
     }
 
-    private final OnClickListener stopListener = new OnClickListener() {
-        @Override
-        public void onClick(View arg0) {
+     private final OnClickListener imageStartListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            status = true;
+            startStreaming();
+        }
+    };
+
+    private final OnClickListener imageStopListener = new View.OnClickListener() {
+        public void onClick(View v) {
             status = false;
             recorder.release();
             Log.d("VS","Recorder released");
         }
-
-    };
-
-    private final OnClickListener startListener = new OnClickListener() {
-        @Override
-        public void onClick(View arg0) {
-            status = true;
-            startStreaming();
-        }
-
     };
 
     public void startStreaming() {
@@ -253,7 +76,10 @@ public class MainActivity extends Activity {
                     byte[] buffer = new byte[minBufSize];
                     Log.d("VS","Buffer created of size " + minBufSize);
                     DatagramPacket packet;
-                    final InetAddress destination = InetAddress.getByName("192.168.0.137");
+                    //final InetAddress destination = InetAddress.getByName("192.168.43.83");
+                    String adr_t = editTextAddress.getText().toString();
+                    final InetAddress destination = InetAddress.getByName(adr_t);
+                    port = Integer.parseInt(editTextPort.getText().toString());
                     Log.d("VS", "Address retrieved");
                     recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,channelConfig,audioFormat,minBufSize*10);
                     Log.d("VS", "Recorder initialized");
@@ -277,132 +103,17 @@ public class MainActivity extends Activity {
         });
         streamThread.start();
     }
-}
-
-
-/*package com.example.myapplication;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    TextView status;
-    InetAddress server_ip;
-    int server_port = 10000;
-
-    String statusText;
-
-    private AsyncTask<Void, Void, Void> async_udp;
-    private boolean Server_Active = true;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        status = (TextView) findViewById(R.id.status);
-    }
-
-    public void sendMessage(View view)  //function bound to message button
-    {
-        runUdpServer();
-    }
-
-    public void refreshStatus(View view)
-    {
-        status.setText(statusText);
-    }
-
-    public void runUdpServer()
-    {
-        int x;
-        status.setText(" ");
-
-        try {
-            server_ip = InetAddress.getByName("192.168.43.xxx"); // ip of THE OTHER DEVICE - NOT THE PHONE
-
-        } catch (UnknownHostException e) {
-            status.append("Error at fetching inetAddress");
-        }
-
-        async_udp = new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-
-                String str2 = "TEST MESSAGE !!!";
-                byte b1[];
-                b1 = new byte[100];
-                b1 = str2.getBytes();
-                //DatagramPacket p1 = new DatagramPacket(b1, b1.length, server_ip, server_port);
-
-                try {
-                    //DatagramSocket s = new DatagramSocket(server_port, server_ip);
-                    DatagramSocket s = new DatagramSocket(server_port);
-                    s.connect(server_ip, server_port);
-
-                    //DatagramPacket p0 = new DatagramPacket(b1, b1.length, InetAddress.getByName("192.168.43.xxx"), server_port);
-                    //s.send(p0);
-                    //The above two line can be used to send a packet - the other code is only to recieve
-
-                    DatagramPacket p1 = new DatagramPacket(b1,b1.length);
-                    s.receive(p1);
-
-                    s.close();
-                    b1=p1.getData();
-                    String str = new String( b1);
-
-                    server_port = p1.getPort();
-                    server_ip=p1.getAddress();
-
-                    String str_msg = "RECEIVED FROM CLIENT IP =" + server_ip + " port=" + server_port + " message no = " + b1[0] +
-                            " data=" + str.substring(1);  //first character is message number
-                    //WARNING: b1 bytes display as signed but are sent as signed characters!
-
-                    //status.setText(str_msg);
-                    statusText = str_msg;
-
-                } catch (SocketException e)
-                {
-                    //status.append("Error creating socket");
-                    statusText.concat(" Error creating socket");   //this doesnt work!
-                } catch (IOException e)
-                {
-                    //status.append("Error recieving packet");
-                    statusText.concat(" Error recieving packet");  //this doesnt work!
-                }
-
-
-                return null;
+    /*public double getAmplitude() {
+        short[] buffer = new short[minSize];
+        ar.read(buffer, 0, minSize);
+        int max = 0;
+        for (short s : buffer)
+        {
+            if (Math.abs(s) > max)
+            {
+                max = Math.abs(s);
             }
-        };
-
-        if (Build.VERSION.SDK_INT >= 11)
-        {
-            async_udp.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
-        else
-        {
-            async_udp.execute();
-        }
-        status.setText(statusText); //need to set out here, as above is in an async thread
-
-    }
-    }
-*/
+        return max;
+    }*/
+}
